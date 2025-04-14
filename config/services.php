77224abc0c724f9d5de4a485c740f695c292bb4b
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-04-14 23:11:09 +0800

    update

--- config/services.php
+++ config/services.php
@@ -2,11 +2,11 @@
 
 return [
     'users1' => [
-        'base_uri' => env('USER1_SERVICE_BASE_URL', 'http://localhost:8000/'),
-        'secret'=> env('USERS1_SERVICE_SECRET'),
+        'base_uri' => env('USER1_SERVICE_BASE_URL', 'https://ddsbe1.onrender.com'),
+        'secret'=> env('USERS1_SERVICE_SECRET', 'F7xqWIp7dg5hpr6fNhk7SeeeybVardOO'),
     ],
     'users2' => [
-        'base_uri' => env('USER2_SERVICE_BASE_URL', 'http://localhost:8001/'),
-        'secret'=> env('USERS2_SERVICE_SECRET'),
+        'base_uri' => env('USER2_SERVICE_BASE_URL', 'https://ddsbe-gateway-b3zq.onrender.com'),
+        'secret'=> env('USERS2_SERVICE_SECRET', 'pwAoG5hQP3S2pCARsBYt7jRMtzw8YEBG'),
     ],
 ];
